dc564e7b6996f51df6b2a5ebe0c64dec31d42a15
Natalia Kazakova (DevExpress) <natalia.kazakova@devexpress.com>
2020-04-17 21:48:57 +0300

    add vb solution

---
+++ VB/Dashboard_PrevYearData/Form1.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Forms
+Imports DevExpress.XtraBars.Ribbon
+
+Namespace Dashboard_PrevYearData
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+			dashboardDesigner1.CreateRibbon()
+			dashboardDesigner1.LoadDashboard("..\..\Data\PrevYearData.xml")
+		End Sub
+	End Class
+End Namespace
