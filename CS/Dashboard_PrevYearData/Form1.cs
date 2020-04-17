3ef6caaac2326e31ef6fbc5aefec788fea2a3deb
Natalia Kazakova (DevExpress) <natalia.kazakova@devexpress.com>
2020-04-17 21:25:05 +0300

    add cs solution

---
+++ CS/Dashboard_PrevYearData/Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+using DevExpress.XtraBars.Ribbon;
+
+namespace Dashboard_PrevYearData {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            dashboardDesigner1.CreateRibbon();
+            dashboardDesigner1.LoadDashboard(@"..\..\Data\PrevYearData.xml");
+        }
+    }
+}
